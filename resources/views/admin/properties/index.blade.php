c654586f005da7012637420df4e8952d856cf680
Trykxx <amitnicolas@gmail.com>
2025-01-20 16:16:01 +0100

    add update-delete-checkbox

--- resources/views/admin/properties/index.blade.php
+++ resources/views/admin/properties/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-center">
         <h1>@yield('title')</h1>
-        <a href="{{ route('admin.property.create')}}" class="btn btn-warning">Ajouter un bien</a>
+        <a href="{{ route('admin.property.create') }}" class="btn btn-warning">Ajouter un bien</a>
     </div>
 
     <table class="table table-striped">
@@ -21,16 +21,24 @@
         <tbody>
             @foreach ($properties as $property)
                 <tr>
-                    <td>{{$property->title}}</td>
-                    <td>{{$property->surface}}m²</td>
-                    <td>{{number_format($property->price, thousands_separator: ' ') }}</td>
+                    <td>{{ $property->title }}</td>
+                    <td>{{ $property->surface }}m²</td>
+                    <td>{{ number_format($property->price, thousands_separator: ' ') }}</td>
+                    <td>{{ $property->city}}</td>
                     <td>
-
+                        <div class="d-flex gap-2 w-100 justify-content-end">
+                            <a href="{{ route('admin.property.edit', $property) }}" class="btn btn-primary">Editer</a>
+                            <form action="{{ route('admin.property.destroy', $property)}}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger">Supprimer</button>
+                            </form>
+                        </div>
                     </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
 
-    {{ $properties->links()}}
+    {{ $properties->links() }}
 @endsection
